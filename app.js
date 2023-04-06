8d2a981ae94504aca10e5e6cc889cb2c3f4df3a9
ABF22 <bofalade@gmail.com>
2023-04-06 18:17:47 +0100

    hi

---
+++ app.js
@@ -0,0 +1,42 @@
+//captilise function//
+function captilise(string){
+   return string.toUpperCase
+}
+//swapcase//
+
+let swapcase=(string)=>{
+    var swapped = "";
+
+  
+  for (var i = 0; i < str.length; i++) {
+    var char = str.charAt(i);
+
+
+    if (char === char.toUpperCase()) {
+      swapped += char.toLowerCase();
+    } else {
+      swapped += char.toUpperCase();
+    }
+  }
+
+  return swapped;
+}
+//reverse// 
+function reverse(string){
+    let listed = string.spilt('')
+    listed.reverse
+    return listed.join('')
+}
+
+//upcase function//
+function upcase(lst){
+    let upped = lst.array.forEach(element => {
+       return element.toUpperCase
+    });
+}
+//downcase//
+function downcase(lst){
+    let downed = lst.array.forEach(element => {
+       return element.toLowerCase
+    })
+
